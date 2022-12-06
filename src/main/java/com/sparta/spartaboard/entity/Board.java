761de381820a0e592e98cc3ba9dd4c3cddf2345c
PriceHoon <saog3151@gmail.com>
2022-12-06 19:56:27 +0900

    first commit

---
+++ src/main/java/com/sparta/spartaboard/entity/Board.java
@@ -0,0 +1,40 @@
+package com.sparta.spartaboard.entity;
+
+//import com.sparta.noticeboard.dto.BoardRequestDTO;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Board extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String pwd;
+
+    @Column(nullable = false)
+    private String contents;
+
+//    public Board(BoardRequestDTO boardDto) {
+//        this.title = boardDto.getTitle();
+//        this.username = boardDto.getUsername();
+//        this.pwd = boardDto.getPwd();
+//        this.contents = boardDto.getContents();
+//    }
+//    public void update(BoardRequestDTO boardDto){
+//        this.title = boardDto.getTitle();
+//        this.username = boardDto.getUsername();
+//        this.contents = boardDto.getContents();
+//    }
+}
