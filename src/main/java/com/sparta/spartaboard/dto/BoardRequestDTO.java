fa906dd8f1c0b1df7c4f33d1419f475e1d4bcc74
PriceHoon <saog3151@gmail.com>
2022-12-06 20:00:28 +0900

    second commit

---
+++ src/main/java/com/sparta/spartaboard/dto/BoardRequestDTO.java
@@ -0,0 +1,13 @@
+package com.sparta.spartaboard.dto;
+
+import lombok.Getter;
+
+@Getter
+public class BoardRequestDTO {
+    private String title;
+    private String username;
+    private String contents;
+    private String pwd;
+
+
+}
