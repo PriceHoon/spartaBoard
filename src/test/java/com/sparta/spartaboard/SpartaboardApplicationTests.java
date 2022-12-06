d36dd158202f01bd17f73b77a9750616fb5553a8
PriceHoon <saog3151@gmail.com>
2022-12-06 19:37:33 +0900

    first commit

---
+++ src/test/java/com/sparta/spartaboard/SpartaboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.spartaboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
