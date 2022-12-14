18fac965bbf61bfb1be9004b83ef03018e8fbe67
PriceHoon <saog3151@gmail.com>
2022-12-14 20:58:49 +0900

    ReadMe,UserController fix

--- src/main/java/com/sparta/spartaboard/controller/UserController.java
+++ src/main/java/com/sparta/spartaboard/controller/UserController.java
@@ -10,9 +10,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/user")
 public class UserController {
 
     private final UserService userService;
